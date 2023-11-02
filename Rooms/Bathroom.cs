920d6aa0f3a2ad1957aea950d819dfc2d82c12ba
bstuff <arthur.bohn@lcieducation.net>
2023-11-02 08:39:22 -0400

    ajout de vapeur
    on doit maintenant prendre un bain pour que le code s'affiche sur le miroir

--- Rooms/Bathroom.cs
+++ Rooms/Bathroom.cs
@@ -7,16 +7,24 @@
 Le [miroir] devant toi affiche ton visage déprimé.
 Tu peux revenir dans ta [chambre].
 ";
-
+        internal static bool bain = false;
         internal override void ReceiveChoice(string choice)
+
         {
             switch (choice)
             {
                 case "bain":
-                    Console.WriteLine("Tu te laisses relaxer dans le bain.");
+                    Console.WriteLine("Tu te laisses relaxer dans le bain. De la vapeur remplit les toilettes.");
+                    bain= true;
                     break;
                 case "miroir":
-                    Console.WriteLine("Tu aperçois les chiffres 2314 écrits sur la brume sur le miroir.");
+                    if (bain == false)
+                    {
+                        Console.WriteLine("Tu regardes ton reflet dans le miroir, et tu as une sale mine.");
+                    }
+                    else if (bain=true) { 
+                    Console.WriteLine("Tu aperçois les chiffres 6969 écrits sur la brume sur le miroir.");
+                    }
                     break;
                 case "chambre":
                     Console.WriteLine("Tu retournes dans ta chambre.");
