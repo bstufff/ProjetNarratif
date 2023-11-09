76cc694d2ba087203d2a877d95b75cace286030b
bstuff <arthur.bohn@lcieducation.net>
2023-11-09 10:45:00 -0500

    Idée 1

--- Program.cs
+++ Program.cs
@@ -2,16 +2,27 @@
 using ProjetNarratif.Rooms;
 
 var game = new Game();
-game.Add(new Bedroom());
+Console.WriteLine(@" __   __  _______  ______    _______  __   __  __   __  _______    _______  _______  ___      _______  ______   
+|  |_|  ||       ||    _ |  |       ||  | |  ||  | |  ||       |  |       ||       ||   |    |       ||    _ |  
+|       ||   _   ||   | ||  |_     _||  | |  ||  | |  ||  _____|  |       ||   _   ||   |    |   _   ||   | ||  
+|       ||  | |  ||   |_||_   |   |  |  |_|  ||  |_|  || |_____   |       ||  | |  ||   |    |  | |  ||   |_||_ 
+|       ||  |_|  ||    __  |  |   |  |       ||       ||_____  |  |      _||  |_|  ||   |___ |  |_|  ||    __  |
+| ||_|| ||       ||   |  | |  |   |  |       ||       | _____| |  |     |_ |       ||       ||       ||   |  | |
+|_|   |_||_______||___|  |_|  |___|  |_______||_______||_______|  |_______||_______||_______||_______||___|  |_|
+"
+
+    );
+game.Add(new ProjetNarratif.Rooms.White());
 game.Add(new Bathroom());
 game.Add(new AtticRoom());
+game.Add(new LivingRoom());
+game.Add(new Phone());
 
 while (!game.IsGameOver())
 {
     Console.WriteLine("--");
     Console.WriteLine(game.CurrentRoomDescription);
     string? choice = Console.ReadLine()?.ToLower() ?? "";
-    Console.Clear();
     game.ReceiveChoice(choice);
 }
 
---
+++ Rooms/White.cs
@@ -0,0 +1,47 @@
+﻿namespace ProjetNarratif.Rooms
+{
+    internal class White : Room
+    {
+        internal static int progression = 0;
+        internal override string CreateDescription() {
+            switch (progression) {
+                case 1:
+                    {
+                        return @"Tu te situes dans une salle complètement blanche.
+Un [téléphone] est sur un bureau devant toi. 
+Un mur s'est déplacé et permet l'accès à une salle [rouge]";
+
+                    }
+                default:
+                    {
+                        return @"Tu te situes dans une salle complètement blanche.
+Un [téléphone] est sur un bureau devant toi. ";
+                    }
+            }
+
+
+}
+        
+        internal override void  ReceiveChoice(string choice)
+        {
+            switch (choice)
+            {
+                case "téléphone":
+                    Game.Transition<Phone>();
+                    break;
+                case "rouge":
+                    if (White.progression >= 1) {
+                        Console.WriteLine("");
+                    }
+                    break;
+                case "grenier":
+                    Console.WriteLine("Tu montes dans le grenier.");
+                    Game.Transition<AtticRoom>();
+                    break;
+                default:
+                    Console.WriteLine("Commande invalide.");
+                    break;
+            }
+        }
+    }
+}
